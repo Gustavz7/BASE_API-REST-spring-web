0d00fac869aba47468199a88c2b371c3816ff24f
Guz_dev <guz.developer@gmail.com>
2024-08-14 17:44:22 -0400

    proyecto inicial, dependencias incluidas

---
+++ src/test/java/com/guz/apirest/ApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guz.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
